967ea7df5fc2c35c41a2198db9bf19493abcec08
Cecilia <earwen1986@gmail.com>
2021-03-17 10:46:16 +0000

    Minor formatting

--- src/CountryMain.js
+++ src/CountryMain.js
@@ -1,23 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const CountryMain = (props) => {
-	const {name, population, region, capital, flag, alpha3Code, isLight} = props;
-	return (
-		<article className={isLight ? "countryMain" : "countryMainDark"}>
-			<Link to={`/country/${alpha3Code}`}>
-			<figure className="mainFigure">
-				<img className="mainFlag" src={flag} alt={name}></img>
-			</figure>
-			</Link>
-			<div className="mainInfo">
-				<h3>{name}</h3>
-				<p><strong>Population: </strong>{population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-				<p><strong>Region: </strong>{region}</p>
-				<p><strong>Capital: </strong>{capital}</p>
-			</div>
-		</article>
-	);
-}
+const CountryMain = ({
+  name,
+  population,
+  region,
+  capital,
+  flag,
+  alpha3Code,
+  isLight,
+}) => {
+  return (
+    <article className={isLight ? "countryMain" : "countryMainDark"}>
+      <Link to={`/country/${alpha3Code}`}>
+        <figure className="mainFigure">
+          <img className="mainFlag" src={flag} alt={name}></img>
+        </figure>
+      </Link>
+      <div className="mainInfo">
+        <h3>{name}</h3>
+        <p>
+          <strong>Population: </strong>
+          {population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+        </p>
+        <p>
+          <strong>Region: </strong>
+          {region}
+        </p>
+        <p>
+          <strong>Capital: </strong>
+          {capital}
+        </p>
+      </div>
+    </article>
+  );
+};
 
 export default CountryMain;
--- src/DetailView.js
+++ src/DetailView.js
@@ -1,52 +1,89 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 
-import countries from './data/countriesAll.json';
+import countries from "./data/countriesAll.json";
 
-const DetailView = ({isLight}) => {
-	const [country, setCountry] = useState('');
-	const { id } = useParams();
+const DetailView = ({ isLight }) => {
+  const [country, setCountry] = useState("");
+  const { id } = useParams();
 
-	useEffect(() => {
-		const newCountry = countries.find((country) => country.alpha3Code === id);
-		setCountry(newCountry);
-	}, [id]);
+  useEffect(() => {
+    const newCountry = countries.find((country) => country.alpha3Code === id);
+    setCountry(newCountry);
+  }, [id]);
 
-	return (
-		<>
-		<Link to='/' >
-			<button className={isLight ? "backButton" : "darkBackButton"}>&larr; &nbsp; Back</button>
-		</Link>
-		{country && 
-			<section className="detailMain">
-			<figure className="detailFigure">
-				<img className="detailFlag" src={country.flag} alt={country.name}></img>
-			</figure>
-			<article className="mainInfo">
-				<h2>{country.name}</h2>
-				<div className="detailContainer">
-					<div>
-						<p><strong>Native name: </strong>{country.nativeName}</p>
-						<p><strong>Population: </strong>{country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-						<p><strong>Region: </strong>{country.region}</p>
-						<p><strong>Subregion: </strong>{country.subregion}</p>
-						<p><strong>Capital: </strong>{country.capital}</p>
-					</div>
-					<div>
-						<p><strong>Top Level Domain: </strong>{country.topLevelDomain}</p>
-						<p><strong>Currencies: </strong>{country.currencies.map(el => el.name).join(' - ')}</p>
-						<p><strong>Languages: </strong>{country.languages.map(el => el.name).join(', ')}</p>
-					</div>
-				</div>
-				<p className="borders">
-					<p><strong>Border countries: </strong></p>
-					{country.borders.map(el => <Link key={countries.find((country) => country.alpha3Code === el).alpha3Code} to={`/country/${countries.find((country) => country.alpha3Code === el).alpha3Code}`}><p className="bordering">{countries.find((country) => country.alpha3Code === el).name}</p></Link>)}
-				</p>
-			</article>
-			</section>
-		}		
-		</>
-	);
-}
+  return (
+    <>
+      <Link to="/">
+        <button className={isLight ? "backButton" : "darkBackButton"}>
+          &larr; &nbsp; Back
+        </button>
+      </Link>
+      {country && (
+        <section className="detailMain">
+          <figure className="detailFigure">
+            <img
+              className="detailFlag"
+              src={country.flag}
+              alt={country.name}
+            ></img>
+          </figure>
+          <article className="mainInfo">
+            <h2>{country.name}</h2>
+            <div className="detailContainer">
+              <div>
+                <p>
+                  <strong>Native name: </strong>
+                  {country.nativeName}
+                </p>
+                <p>
+                  <strong>Population: </strong>
+                  {country.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                </p>
+                <p>
+                  <strong>Region: </strong>
+                  {country.region}
+                </p>
+                <p>
+                  <strong>Subregion: </strong>
+                  {country.subregion}
+                </p>
+                <p>
+                  <strong>Capital: </strong>
+                  {country.capital}
+                </p>
+              </div>
+              <div>
+                <p>
+                  <strong>Top Level Domain: </strong>
+                  {country.topLevelDomain}
+                </p>
+                <p>
+                  <strong>Currencies: </strong>
+                  {country.currencies.map((el) => el.name).join(" - ")}
+                </p>
+                <p>
+                  <strong>Languages: </strong>
+                  {country.languages.map((el) => el.name).join(", ")}
+                </p>
+              </div>
+            </div>
+            <p className="borders">
+              <p><strong>Border countries: </strong></p>
+              {country.borders.map((el) => (
+                <Link
+                  key={countries.find((country) => country.alpha3Code === el).alpha3Code}
+                  to={`/country/${countries.find((country) => country.alpha3Code === el).alpha3Code}`}
+                >
+                  <p className="bordering">{countries.find((country) => country.alpha3Code === el).name}</p>
+                </Link>
+              ))}
+            </p>
+          </article>
+        </section>
+      )}
+    </>
+  );
+};
 
 export default DetailView;
