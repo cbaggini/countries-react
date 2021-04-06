3faef6449ae648ab8f9c3ad0294e0036260bbe03
Cecilia <earwen1986@gmail.com>
2021-04-06 21:07:11 +0100

    Simplified search and filtering by region

--- src/CountryView.js
+++ src/CountryView.js
@@ -1,37 +1,52 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-import countries from './data/countriesAll.json';
-import CountryMain from './CountryMain';
-import SearchBar from './SearchBar';
+import countries from "./data/countriesAll.json";
+import CountryMain from "./CountryMain";
+import SearchBar from "./SearchBar";
 
-const CountryView = ({isLight}) => {
-	const [filteredCountries, setFilteredCountries] = useState(countries);
-	const regions = [...new Set(countries.map(el => el.region).filter(el => el.length > 1))];
-	const [selectedRegions, setSelectedRegions] = useState(regions);
-	const [search, setSearch] = useState('');
+const CountryView = ({ isLight }) => {
+  const regions = [
+    ...new Set(countries.map((el) => el.region).filter((el) => el.length > 1)),
+  ];
+  const [selectedRegions, setSelectedRegions] = useState("All");
+  const [search, setSearch] = useState("");
 
-	const filterCountries = (e) => {
-		if (e.target.value) {
-			const newCountries = countries.filter(el => (el.name.toLowerCase().includes(e.target.value.toLowerCase()) || el.capital.toLowerCase().includes(e.target.value.toLowerCase())) && selectedRegions.includes(el.region));
-			setFilteredCountries(newCountries);
-			setSearch(e.target.value);
-		} else {
-			const newCountries = countries.filter(el => selectedRegions.includes(el.region))
-			setFilteredCountries(newCountries);
-			setSearch('');
-		}	
-	}
+  const filteredCountries = countries.filter((country) => {
+    if (selectedRegions === "All") {
+      return (
+        country.name.toLowerCase().includes(search) ||
+        country.capital.toLowerCase().includes(search)
+      );
+    } else {
+      return (
+        (country.name.toLowerCase().includes(search) ||
+          country.capital.toLowerCase().includes(search)) &&
+        selectedRegions.includes(country.region)
+      );
+    }
+  });
 
-	return (
-		<>
-		<SearchBar isLight={isLight} filterCountries={filterCountries} regions={regions} setSelectedRegions={setSelectedRegions} search={search} setFilteredCountries={setFilteredCountries}/>
-		<section className="countriesContainer">
-			{filteredCountries.map((country) => {
-				return (<CountryMain key={country.alpha3Code} {... country} isLight={isLight}/>)
-			})}
-		</section>
-		</>
-	);
-}
+  return (
+    <>
+      <SearchBar
+        isLight={isLight}
+        regions={regions}
+        setSelectedRegions={setSelectedRegions}
+        setSearch={setSearch}
+      />
+      <section className="countriesContainer">
+        {filteredCountries.map((country) => {
+          return (
+            <CountryMain
+              key={country.alpha3Code}
+              {...country}
+              isLight={isLight}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
+};
 
 export default CountryView;
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,34 +1,41 @@
 import React from "react";
 
-import countries from './data/countriesAll.json';
+const SearchBar = ({ regions, setSelectedRegions, setSearch, isLight }) => {
+  const toggleRegions = (e) => {
+    setSelectedRegions(e.target.value);
+  };
 
-const SearchBar = ({filterCountries, regions, setSelectedRegions, search, setFilteredCountries, isLight}) => {
+  const filterCountries = (e) => {
+    setSearch(e.target.value.toLowerCase());
+  };
 
-	const toggleRegions = e => {
-		if (e.target.value) {
-			setSelectedRegions([e.target.value]);
-			const newCountries = countries.filter(el => (el.name.toLowerCase().includes(search.toLowerCase()) || el.capital.toLowerCase().includes(search.toLowerCase())) && el.region === e.target.value);
-			setFilteredCountries(newCountries);
-		} else {
-			setSelectedRegions(regions);
-			const newCountries = countries.filter(el => el.name.toLowerCase().includes(search.toLowerCase()) || el.capital.toLowerCase().includes(search.toLowerCase()));
-			setFilteredCountries(newCountries);
-		}
-		
-	}
-
-	return (
-		<nav className="filters">
-			<div className={isLight ? "search" : "searchDark"}>
-				<i className="fa fa-search" aria-hidden="true" style={{color:"lightgrey"}}></i>
-				<input type="text" placeholder="Search for a country..." onChange={filterCountries}></input>
-			</div>
-			<select className={isLight ? "regions" : "regionsDark"} onChange={toggleRegions}>
-				<option value="">Filter By Region</option>
-				{regions.map(el => <option key={el} value={el}>{el}</option>)}
-			</select>
-		</nav>
-	);
-}
+  return (
+    <nav className="filters">
+      <div className={isLight ? "search" : "searchDark"}>
+        <i
+          className="fa fa-search"
+          aria-hidden="true"
+          style={{ color: "lightgrey" }}
+        ></i>
+        <input
+          type="text"
+          placeholder="Search for a country..."
+          onChange={filterCountries}
+        ></input>
+      </div>
+      <select
+        className={isLight ? "regions" : "regionsDark"}
+        onChange={toggleRegions}
+      >
+        <option value="All">Filter By Region</option>
+        {regions.map((el) => (
+          <option key={el} value={el}>
+            {el}
+          </option>
+        ))}
+      </select>
+    </nav>
+  );
+};
 
 export default SearchBar;
